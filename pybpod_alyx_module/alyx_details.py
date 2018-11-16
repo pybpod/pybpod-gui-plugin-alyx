31de95e637f3b27479ecd0f8c082e86ee0f3f7cb
Luís Teixeira <micboucinha@gmail.com>
2018-11-16 13:19:03 +0000

    Added the more detailed information to the Alyx subject and details

--- pybpod_alyx_module/alyx_details.py
+++ pybpod_alyx_module/alyx_details.py
@@ -16,13 +16,13 @@ class AlyxDetails(AlyxModule, BaseWidget):
         self._nickname = ControlLabel(_subject.name)
         self._nickname.selectable = True
 
-        self._id_text = ControlLabel('ID:')
-        self._id = ControlLabel(_subject.alyx_id)
-        self._id.selectable = True
-
         # self._url_text = ControlLabel('URL:')
         # self._url = ControlLabel(_subject.alyx_url)
         # self._url.selectable = True
+
+        self._id_text = ControlLabel('ID:')
+        self._id = ControlLabel(_subject.alyx_id)
+        self._id.selectable = True
 
         self._responsible_user_text = ControlLabel('Responsible user:')
         self._responsible_user = ControlLabel(_subject.alyx_responsible_user)
@@ -31,6 +31,10 @@ class AlyxDetails(AlyxModule, BaseWidget):
         self._birth_text = ControlLabel('Birth date:')
         self._birth = ControlLabel(_subject.alyx_birth_date)
         self._birth.selectable = True
+
+        self._age_weeks_text = ControlLabel('Age (weeks):')
+        self._age_weeks = ControlLabel(str(_subject.alyx_age_weeks))
+        self._age_weeks.selectable = True
 
         self._death_text = ControlLabel('Death date:')
         self._death = ControlLabel(_subject.alyx_death_date)
@@ -52,13 +56,17 @@ class AlyxDetails(AlyxModule, BaseWidget):
         self._strain = ControlLabel(_subject.alyx_strain)
         self._strain.selectable = True
 
+        self._source_text = ControlLabel('Source:')
+        self._source = ControlLabel(_subject.alyx_source)
+        self._source.selectable = True
+
         self._line_text = ControlLabel('Line:')
         self._line = ControlLabel(_subject.alyx_line)
         self._line.selectable = True
 
-        self._description_text = ControlLabel('Description:')
-        self._description = ControlLabel(_subject.alyx_description)
-        self._description.selectable = True
+        self._projects_text = ControlLabel('Projects:')
+        self._projects = ControlLabel(", ".join(map(str, _subject.alyx_projects)) if _subject.alyx_projects else None)
+        self._projects.selectable = True
 
         self._lab_text = ControlLabel('Lab:')
         self._lab = ControlLabel(_subject.alyx_lab)
@@ -68,33 +76,60 @@ class AlyxDetails(AlyxModule, BaseWidget):
         self._genotype = ControlLabel(", ".join(map(str, _subject.alyx_genotype)) if _subject.alyx_genotype else None)
         self._genotype.selectable = True
 
-        self._alive_text = ControlLabel('Alive:')
-        self._alive = ControlLabel(str(_subject.alyx_alive))
-        self._alive.selectable = True
+        self._description_text = ControlLabel('Description:')
+        self._description = ControlLabel(_subject.alyx_description)
+        self._description.selectable = True
 
-        self._projects_text = ControlLabel('Projects:')
-        self._projects = ControlLabel(", ".join(map(str, _subject.alyx_projects)) if _subject.alyx_projects else None)
-        self._projects.selectable = True
+        self._weighings_text = ControlLabel('Weighings:')
+        self._weighings = ControlLabel(", ".join(map(str, _subject.alyx_weighings)) if _subject.alyx_weighings else None)
+        self._weighings.selectable = True
+
+        self._reference_weight_text = ControlLabel('Reference weight:')
+        self._reference_weight = ControlLabel(_subject.alyx_reference_weight)
+        self._reference_weight.selectable = True
+
+        self._water_administrations_text = ControlLabel('Water admninistrations:')
+        self._water_administrations = ControlLabel( ", ".join(map(str, _subject.alyx_water_administrations)) if _subject.alyx_water_administrations else None)
+        self._water_administrations.selectable = True
+
+        self._last_water_restriction_text = ControlLabel('Last water restriction:')
+        self._last_water_restriction = ControlLabel(_subject.alyx_last_water_restriction)
+        self._last_water_restriction.selectable = True
+
+        self._expected_water_text = ControlLabel('Expected water:')
+        self._expected_water = ControlLabel(str(_subject.alyx_expected_water))
+        self._expected_water.selectable = True
+
+        self._remaining_water_text = ControlLabel('Remaining water:')
+        self._remaining_water = ControlLabel(str(_subject.alyx_remaining_water))
+        self._remaining_water.selectable = True
 
         self.set_margin(10)
 
         self.formset = [
             ('_nickname_text', '_nickname'),
-            ('_id_text', '_id'),
             # ('_url_text', '_url'),
+            ('_id_text', '_id'),
             ('_responsible_user_text', '_responsible_user'),
             ('_birth_text', '_birth'),
+            ('_age_weeks_text', '_age_weeks'),
             ('_death_text', '_death'),
             ('_species_text', '_species'),
             ('_sex_text', '_sex'),
             ('_litter_text', '_litter'),
             ('_strain_text', '_strain'),
+            ('_source_text', '_source'),
             ('_line_text', '_line'),
-            ('_description_text', '_description'),
+            ('_projects_text', '_projects'),
             ('_lab_text', '_lab'),
             ('_genotype_text', '_genotype'),
-            ('_alive_text', '_alive'),
-            ('_projects_text', '_projects')
+            ('_description_text', '_description'),
+            ('_weighings_text', '_weighings'),
+            ('_reference_weight_text', '_reference_weight'),
+            ('_water_administrations_text', '_water_administrations'),
+            ('_last_water_restriction_text', '_last_water_restriction'),
+            ('_expected_water_text', '_expected_water'),
+            ('_remaining_water_text', '_remaining_water'),
         ]
 
     def keyPressEvent(self, event: QtGui.QKeyEvent):
--- pybpod_alyx_module/models/subject/alyx_subject.py
+++ pybpod_alyx_module/models/subject/alyx_subject.py
@@ -19,23 +19,29 @@ class AlyxSubject(SubjectUIBusy):
         # print(self.uuid4)
         self.name = jsondata['nickname']
         self.alyx_nickname = jsondata['nickname']
-        self.alyx_id = jsondata['id']
         self.alyx_url = jsondata['url']
+        self.alyx_id = jsondata['id']
         self.alyx_responsible_user = jsondata['responsible_user']
         self.alyx_birth_date = jsondata['birth_date']
+        self.alyx_age_weeks = jsondata['age_weeks']
         self.alyx_death_date = jsondata['death_date']
         self.alyx_species = jsondata['species']
         self.alyx_sex = jsondata['sex']
         self.alyx_litter = jsondata['litter']
         self.alyx_strain = jsondata['strain']
+        self.alyx_source = jsondata['source']
         self.alyx_line = jsondata['line']
-        self.alyx_description = jsondata['description']
+        self.alyx_projects = jsondata['projects']
         self.alyx_lab = jsondata['lab']
         self.alyx_genotype = jsondata['genotype']
-        self.alyx_alive = "Yes" if self.alyx_death_date is None else "No"
-        self.alyx_projects = jsondata['projects']
+        self.alyx_description = jsondata['description']
+        self.alyx_weighings = jsondata['weighings']
+        self.alyx_water_administrations = jsondata['water_administrations']
+        self.alyx_reference_weight = jsondata['reference_weight']
+        self.alyx_last_water_restriction = jsondata['last_water_restriction']
+        self.alyx_expected_water = jsondata['expected_water']
+        self.alyx_remaining_water = jsondata['remaining_water']
 
-        # print(self.uuid4)
         if not hasattr(self, '_alyx_menu'):
             self._alyx_menu = self._tree.add_popup_menu_option('Alyx Details', self.showdetails, item=self.node)
         self._name.readonly = True
@@ -68,21 +74,28 @@ class AlyxSubject(SubjectUIBusy):
                     def_text='This file contains information about a subject used on PyBpod GUI.',
                 )
             data['nickname'] = self.alyx_nickname
-            data['alyx_id'] = self.alyx_id
             data['url'] = self.alyx_url
+            data['alyx_id'] = self.alyx_id
             data['responsible_user'] = self.alyx_responsible_user
             data['birth_date'] = self.alyx_birth_date
+            data['age_weeks'] = self.alyx_age_weeks
             data['death_date'] = self.alyx_death_date
             data['species'] = self.alyx_species
             data['sex'] = self.alyx_sex
             data['litter'] = self.alyx_litter
             data['strain'] = self.alyx_strain
+            data['source'] = self.alyx_source
             data['line'] = self.alyx_line
-            data['description'] = self.alyx_description
+            data['projects'] = self.alyx_projects
             data['lab'] = self.alyx_lab
             data['genotype'] = self.alyx_genotype
-            data['alive'] = self.alyx_alive
-            data['projects'] = self.alyx_projects
+            data['description'] = self.alyx_description
+            data['weighings'] = self.alyx_weighings
+            data['water_administration'] = self.alyx_water_administrations
+            data['reference_weight'] = self.alyx_reference_weight
+            data['last_water_restriction'] = self.alyx_last_water_restriction
+            data['expected_water'] = self.alyx_expected_water
+            data['remaining_water'] = self.alyx_remaining_water
 
             config_path = os.path.join(self.path, self.name+'.json')
             with open(config_path, 'w') as fstream: json.dump(data, fstream)
@@ -100,21 +113,28 @@ class AlyxSubject(SubjectUIBusy):
         data['name'] = self.name
         data['uuid4'] = self.uuid4
         data['nickname'] = self.alyx_nickname
-        data['alyx_id'] = self.alyx_id
         data['url'] = self.alyx_url
+        data['alyx_id'] = self.alyx_id
         data['responsible_user'] = self.alyx_responsible_user
         data['birth_date'] = self.alyx_birth_date
+        data['age_weeks'] = self.alyx_age_weeks
         data['death_date'] = self.alyx_death_date
         data['species'] = self.alyx_species
         data['sex'] = self.alyx_sex
         data['litter'] = self.alyx_litter
         data['strain'] = self.alyx_strain
+        data['source'] = self.alyx_source
         data['line'] = self.alyx_line
-        data['description'] = self.alyx_description
+        data['projects'] = self.alyx_projects
         data['lab'] = self.alyx_lab
         data['genotype'] = self.alyx_genotype
-        data['alive'] = self.alyx_alive
-        data['projects'] = self.alyx_projects
+        data['description'] = self.alyx_description
+        data['weighings'] = self.alyx_weighings
+        data['water_administration'] = self.alyx_water_administrations
+        data['reference_weight'] = self.alyx_reference_weight
+        data['last_water_restriction'] = self.alyx_last_water_restriction
+        data['expected_water'] = self.alyx_expected_water
+        data['remaining_water'] = self.alyx_remaining_water
 
         return json.dumps(data)
 
@@ -136,24 +156,30 @@ class AlyxSubject(SubjectUIBusy):
                 super().load(path)
                 return
 
-            #self.name = data.get('name', None)
             self.uuid4 = data.uuid4 if data.uuid4 else self.uuid4
             self.alyx_nickname = data.get('nickname', None)
-            self.alyx_id = data.get('alyx_id', None)
             self.alyx_url = data.get('url', None)
+            self.alyx_id = data.get('alyx_id', None)
             self.alyx_responsible_user = data.get('responsible_user', None)
             self.alyx_birth_date = data.get('birth_date', None)
+            self.alyx_age_weeks = data.get('age_weeks', None)
             self.alyx_death_date = data.get('death_date', None)
             self.alyx_species = data.get('species', None)
             self.alyx_sex = data.get('sex', None)
             self.alyx_litter = data.get('litter', None)
             self.alyx_strain = data.get('strain', None)
+            self.alyx_source = data.get('source', None)
             self.alyx_line = data.get('line', None)
-            self.alyx_description = data.get('description', None)
+            self.alyx_projects = data.get('projects', None)
             self.alyx_lab = data.get('lab', None)
             self.alyx_genotype = data.get('genotype', None)
-            self.alyx_alive = data.get('alive', None)
-            self.alyx_projects = data.get('projects', None)
+            self.alyx_description = data.get('description', None)
+            self.alyx_weighings = data.get('weighings', None)
+            self.alyx_water_administrations = data.get('water_administrations', None)
+            self.alyx_reference_weight = data.get('reference_weight', None)
+            self.alyx_last_water_restriction = data.get('last_water_restriction', None)
+            self.alyx_expected_water = data.get('expected_water', None)
+            self.alyx_remaining_water = data.get('remaining_water', None)
 
             self._alyx_menu = self._tree.add_popup_menu_option('Alyx Details', self.showdetails, item=self.node)
             self._name.readonly = True
@@ -191,40 +217,3 @@ class AlyxSubject(SubjectUIBusy):
             self.detailswindow = AlyxDetails(self)
         self.detailswindow.show()
         return self.detailswindow
-
-'''
-test_subject = {
-    "nickname": "test_subject_posted",
-    "responsible_user": "test_user",  # Required
-    "birth_date": None,
-    "death_date": None,
-    "species": None,
-    "sex": 'U',  # Required
-    "litter": None,
-    "strain": None,
-    "line": None,
-    "description": "Some description",
-    "genotype": []  # Required?
-}
-'''
-
-'''
-{
-    'species': 'mouse', 
-    'genotype': [], 
-    'litter': None, 
-    'alive': True, 
-    'url': 'http://alyx.champalimaud.pt:8000/subjects/4577', 
-    'line': None, 
-    'birth_date': '2017-04-11', 
-    'responsible_user': 'ines', 
-    'sex': 'F', 
-    'death_date': None, 
-    'description': '', 
-    'nickname': '4577', 
-    'strain': 'VGlut-2-ChR2-het', 
-    'id': '27705345-f49a-4483-aec8-313fc01d2c1f'
-} 
-
-
-'''     
