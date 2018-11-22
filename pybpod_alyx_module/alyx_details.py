faed17652580ab1cbbb84415f65ab854a3261d3e
Luís Teixeira <micboucinha@gmail.com>
2018-11-22 10:16:04 +0000

    Moved selectable to the constructor call

--- pybpod_alyx_module/alyx_details.py
+++ pybpod_alyx_module/alyx_details.py
@@ -14,96 +14,73 @@ class AlyxDetails(AlyxModule, BaseWidget):
         AlyxModule.__init__(self)
 
         self._nickname_text = ControlLabel('Nickname:')
-        self._nickname = ControlLabel(_subject.name)
-        self._nickname.selectable = True
+        self._nickname = ControlLabel(_subject.name, selectable=True)
 
         # self._url_text = ControlLabel('URL:')
-        # self._url = ControlLabel(_subject.alyx_url)
-        # self._url.selectable = True
+        # self._url = ControlLabel(_subject.alyx_url, selectable=True)
 
         self._id_text = ControlLabel('ID:')
-        self._id = ControlLabel(_subject.alyx_id)
-        self._id.selectable = True
+        self._id = ControlLabel(_subject.alyx_id, selectable=True)
 
         self._responsible_user_text = ControlLabel('Responsible user:')
-        self._responsible_user = ControlLabel(_subject.alyx_responsible_user)
-        self._responsible_user.selectable = True
+        self._responsible_user = ControlLabel(_subject.alyx_responsible_user, selectable=True)
 
         self._birth_text = ControlLabel('Birth date:')
-        self._birth = ControlLabel(_subject.alyx_birth_date)
-        self._birth.selectable = True
+        self._birth = ControlLabel(_subject.alyx_birth_date, selectable=True)
 
         self._age_weeks_text = ControlLabel('Age (weeks):')
-        self._age_weeks = ControlLabel(str(_subject.alyx_age_weeks))
-        self._age_weeks.selectable = True
+        self._age_weeks = ControlLabel(str(_subject.alyx_age_weeks), selectable=True)
 
         self._death_text = ControlLabel('Death date:')
-        self._death = ControlLabel(_subject.alyx_death_date)
-        self._death.selectable = True
+        self._death = ControlLabel(_subject.alyx_death_date, selectable=True)
 
         self._species_text = ControlLabel('Species:')
-        self._species = ControlLabel(_subject.alyx_species)
-        self._species.selectable = True
+        self._species = ControlLabel(_subject.alyx_species, selectable=True)
 
         self._sex_text = ControlLabel('Sex:')
-        self._sex = ControlLabel(_subject.alyx_species)
-        self._sex.selectable = True
+        self._sex = ControlLabel(_subject.alyx_species, selectable=True)
 
         self._litter_text = ControlLabel('Litter:')
-        self._litter = ControlLabel(_subject.alyx_litter)
-        self._litter.selectable = True
+        self._litter = ControlLabel(_subject.alyx_litter, selectable=True)
 
         self._strain_text = ControlLabel('Strain:')
-        self._strain = ControlLabel(_subject.alyx_strain)
-        self._strain.selectable = True
+        self._strain = ControlLabel(_subject.alyx_strain, selectable=True)
 
         self._source_text = ControlLabel('Source:')
-        self._source = ControlLabel(_subject.alyx_source)
-        self._source.selectable = True
+        self._source = ControlLabel(_subject.alyx_source, selectable=True)
 
         self._line_text = ControlLabel('Line:')
-        self._line = ControlLabel(_subject.alyx_line)
-        self._line.selectable = True
+        self._line = ControlLabel(_subject.alyx_line, selectable=True)
 
         self._projects_text = ControlLabel('Projects:')
-        self._projects = ControlLabel(", ".join(map(str, _subject.alyx_projects)) if _subject.alyx_projects else None)
-        self._projects.selectable = True
+        self._projects = ControlLabel(", ".join(map(str, _subject.alyx_projects)) if _subject.alyx_projects else None, selectable=True)
 
         self._lab_text = ControlLabel('Lab:')
-        self._lab = ControlLabel(_subject.alyx_lab)
-        self._lab.selectable = True
+        self._lab = ControlLabel(_subject.alyx_lab, selectable=True)
 
         self._genotype_text = ControlLabel('Genotype:')
-        self._genotype = ControlLabel(", ".join(map(str, _subject.alyx_genotype)) if _subject.alyx_genotype else None)
-        self._genotype.selectable = True
+        self._genotype = ControlLabel(", ".join(map(str, _subject.alyx_genotype)) if _subject.alyx_genotype else None, selectable=True)
 
         self._description_text = ControlLabel('Description:')
-        self._description = ControlLabel(_subject.alyx_description)
-        self._description.selectable = True
+        self._description = ControlLabel(_subject.alyx_description, selectable=True)
 
         self._weighings_text = ControlLabel('Weighings:')
-        self._weighings = ControlLabel(", ".join(map(str, _subject.alyx_weighings)) if _subject.alyx_weighings else None)
-        self._weighings.selectable = True
+        self._weighings = ControlLabel(", ".join(map(str, _subject.alyx_weighings)) if _subject.alyx_weighings else None, selectable=True)
 
         self._reference_weight_text = ControlLabel('Reference weight:')
-        self._reference_weight = ControlLabel(str(_subject.alyx_reference_weight))
-        self._reference_weight.selectable = True
+        self._reference_weight = ControlLabel(str(_subject.alyx_reference_weight), selectable=True)
 
         self._water_administrations_text = ControlLabel('Water admninistrations:')
-        self._water_administrations = ControlLabel( ", ".join(map(str, _subject.alyx_water_administrations)) if _subject.alyx_water_administrations else None)
-        self._water_administrations.selectable = True
+        self._water_administrations = ControlLabel(", ".join(map(str, _subject.alyx_water_administrations)) if _subject.alyx_water_administrations else None, selectable=True)
 
         self._last_water_restriction_text = ControlLabel('Last water restriction:')
-        self._last_water_restriction = ControlLabel(_subject.alyx_last_water_restriction)
-        self._last_water_restriction.selectable = True
+        self._last_water_restriction = ControlLabel(_subject.alyx_last_water_restriction, selectable=True)
 
         self._expected_water_text = ControlLabel('Expected water:')
-        self._expected_water = ControlLabel(str(_subject.alyx_expected_water))
-        self._expected_water.selectable = True
+        self._expected_water = ControlLabel(str(_subject.alyx_expected_water), selectable=True)
 
         self._remaining_water_text = ControlLabel('Remaining water:')
-        self._remaining_water = ControlLabel(str(_subject.alyx_remaining_water))
-        self._remaining_water.selectable = True
+        self._remaining_water = ControlLabel(str(_subject.alyx_remaining_water), selectable=True)
 
         self.set_margin(10)
 
