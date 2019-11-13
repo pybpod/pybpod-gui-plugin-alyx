52cb10f9236c82060890e77dcfbfb9dea8d6b099
Luís Teixeira <micboucinha@gmail.com>
2019-11-13 14:22:00 +0000

    Update code style on module.py

--- pybpod_alyx_module/module.py
+++ pybpod_alyx_module/module.py
@@ -2,7 +2,7 @@ from pybpodapi.bpod_modules.bpod_module import BpodModule
 
 
 class Alyx(BpodModule):
-    
+
     @staticmethod
     def check_module_type(module_name):
         return module_name and module_name.startswith('Alyx')
